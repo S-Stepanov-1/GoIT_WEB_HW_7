f93e699e5f6732083fc5eef5ce0211267046c074
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-07-05 23:51:10 +0300

    add models

---
+++ models.py
@@ -0,0 +1,55 @@
+from sqlalchemy import ForeignKey, String, Date
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+
+
+Base = declarative_base()
+
+
+class Group(Base):
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    surname: Mapped[str] = mapped_column(nullable=False)
+
+    @hybrid_property
+    def fullname(self):
+        return f"{self.name} {self.surname}"
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    surname: Mapped[str] = mapped_column(String, nullable=False)
+    group_id: Mapped[int] = mapped_column(ForeignKey("groups.id", ondelete="CASCADE"))
+    group: Mapped["Group"] = relationship("Group", backref="students")
+
+    @hybrid_property
+    def fullname(self):
+        return f"{self.name} {self.surname}"
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("teachers.id", ondelete="CASCADE"))
+    teacher: Mapped["Teacher"] = relationship("Teacher", backref="subjects")
+
+
+class Grade(Base):
+    __tablename__ = "grades"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    grade: Mapped[int] = mapped_column(nullable=False)
+    date_of: Mapped[Date] = mapped_column(nullable=False)
+    student_id: Mapped[int] = mapped_column(ForeignKey("students.id", ondelete="CASCADE"))
+    subject_id: Mapped[int] = mapped_column(ForeignKey("subjects.id", ondelete="CASCADE"))
+    student: Mapped["Student"] = relationship("Student", backref="grade")
+    subject: Mapped["Subject"] = relationship("Subject", backref="grade")
