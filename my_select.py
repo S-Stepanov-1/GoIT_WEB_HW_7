79a0d4b003eb925b2f9718c2ae232f827da3c703
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-07-07 23:16:37 +0300

    add select 6, 7 & 8

--- my_select.py
+++ my_select.py
@@ -86,3 +86,50 @@ def select_5():
     result = session.execute(statement)
     for teacher, subj in result.all():
         print(teacher, "\t", subj)
+
+
+def select_6():
+    """--- Find a list of students in a particular group ---"""
+    print(select_6.__doc__)
+
+    statement = (select(Group.name, Student.fullname)
+                 .join(Student, Student.group_id == Group.id)
+                 .where(Group.id == 1)
+                 .group_by(Student.id)
+                 .group_by(Group.name))
+
+    result = session.execute(statement)
+    for group, student in result.all():
+        print(group, " | ", student)
+
+
+def select_7():
+    """---- Find the grades of students in an individual group in a particular subject ---"""
+    print(select_7.__doc__)
+
+    statement = (select(Subject.name, Group.name, Student.fullname, Grade.grade)
+                 .join(Grade, Grade.subject_id == Subject.id)
+                 .join(Student, Student.id == Grade.student_id)
+                 .join(Group, Group.id == Student.group_id)
+                 .where(Group.id == 1)  # you can change the values for "g.id"...
+                 .where(Subject.id == 1)  # ...and for "subj.id" to get grades for different groups in different subjects
+                 )
+    result = session.execute(statement)
+    for line in result.all():
+        print(line)
+
+
+def select_8():
+    """--- Find the average score that a particular teacher gives in his or her subjects ---"""
+    print(select_8.__doc__)
+
+    statement = (select(Teacher.fullname, Subject.name, func.round(func.avg(Grade.grade), 2))
+                 .join(Subject, Subject.teacher_id == Teacher.id)
+                 .join(Grade, Grade.subject_id == Subject.id)
+                 .group_by(Teacher.id)
+                 .group_by(Subject.name)
+                 .order_by(Teacher.id)
+                 )
+    result = session.execute(statement)
+    for teacher, subject, avg_grade in result.all():
+        print(teacher, subject, avg_grade)
